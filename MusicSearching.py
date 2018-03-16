bf52aeaa690e054921853acda332b86d0a582670
YI LI <982366588@qq.com>
2018-03-16 15:44:50 +0100

    Version 0
    Write the structure. Init of the project

---
+++ MusicSearching.py
@@ -0,0 +1,67 @@
+class FileOperator:
+    def ReadFile(self):
+        # TODO
+        return 0
+
+    def ReadFileXML(self):
+        # TODO
+        return 0
+
+    def ReadFileMEI(self):
+        # TODO
+        return 0
+
+    def SaveNoteList(self):
+        # TODO
+        return 0
+
+    def InitFiles(self):
+        # TODO
+        return 0
+
+class Seperator:
+    def SeperateByPart(self):
+        # TODO
+        return 0
+
+    def SeperateByMeasure(self):
+        # TODO
+        return 0
+
+    def SeperateByNMeasure(self):
+        # TODO
+        return 0
+
+class Translator:
+    def TranslateToNote(self):
+        # TODO
+        return 0
+
+    def TranslateToNoteList(self):
+        # TODO
+        return 0
+
+class DistanceCalculator(slef):
+    def Distance_Note_Note(self, note1, note2):
+        # TODO
+        return 0
+
+    def Distance_Note_Accord(self, note, accord):
+        # TODO
+        return 0
+
+    def Distance_Accord_Accord(self, accord1, accord2):
+        # TODO
+        return 0
+
+    def Distance_Morceau_Morceau(self, morceau, morceau):
+        # TODO
+        return 0
+
+class Controller:
+    def execute(self):
+        # TODO
+        return 0
+
+if __name__ == '__main__':
+    Controller().execute()
