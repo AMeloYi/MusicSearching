67d6c1184217175631aac2a236afd73cdd6623e1
YI LI <982366588@qq.com>
2018-03-21 11:50:00 +0100

    V0. 不是很能用的版本

--- MusicSearching.py
+++ MusicSearching.py
@@ -3,9 +3,43 @@ from music21.exceptions21 import *
 import win32api
 import os
 
-
 ABSPATH = os.path.abspath('.')
 
+class Note:
+    def __init__(self, voix, gamme, valeur, duree):
+        self.voix = voix
+        self.gamme = gamme
+        self.valeur = valeur
+        self.duree = duree
+
+class Accord:
+    def __init__(self, notes):
+        self.notes = []
+        for note in notes:
+            self.notes.append(note)
+
+    def AddNote(note):
+        self.notes.append(note)
+
+class Morceau:
+    def __init__(self, list):
+        self.morceau = []
+        for n in list:
+            self.morceau.append(n)
+
+    def AddElement(e):
+        self.morceau.append(e)
+
+
+# --------------------------------------------------------------------
+# class fileOperator
+# fonction:
+#     - ReadFile
+#     - ReadFileXML
+#     - ReadFileMEI
+#     - SaveNoteList
+#     - InitFiles
+# --------------------------------------------------------------------
 class FileOperator:
     def ReadFile(self, filename):
         if os.path.exists(filename):
@@ -42,68 +76,184 @@ class FileOperator:
         else:
             return None
 
-    def SaveNoteList(self, id, list):
-        string = ''
-        for e in list:
-            string += e
-        filename = ABSPATH + r'\tmp\tmp_' + str(id) + '.csv'
+    def SaveNoteList(self, filename, list):
+        # filename = ABSPATH + r'\tmp\tmp_' + str(id) + '.csv'
         f = open(filename, 'a')
-        f.write(string + '\n')
+        for note in list:
+            string = str(note.voix) + ',' + str(note.gamme) + ',' +  str(note.valeur) + ',' + str(note.duree)
+            f.write(string + '\n')
         f.close()
 
     def InitFiles(self):
-        dirpath = ABSPATH + r'\tmp'
+        dirpath = ABSPATH + r'\tmp\target'
         for (root, dirs, files) in os.walk(dirpath):
             for file in files:
                 filename = os.path.join(root, file)
                 if os.path.exists(filename):
                     os.remove(filename)
 
+# --------------------------------------------------------------------
+# class Seperator:
+# fonction:
+#     - SeperateByPart
+#     - SeperateByMeasure
+#     - SeperateByNMeasure
+#     - SeperateByMorceau
+# --------------------------------------------------------------------
 class Seperator:
-    def SeperateByPart(self):
-        # TODO
-        return 0
+    def SeperateByPart(self, inputStream):
+        list = []
+        for i in range(len(inputStream.parts)):
+            nStream = inputStream.parts[i]
+            list.append(nStream)
+        return len(inputStream.parts), list
 
-    def SeperateByMeasure(self):
-        # TODO
-        return 0
+    def SeperateByMeasure(self, inputStream):
+        nstream = inputStream.recurse().notes.stream()
+        list = []
+        for n in nstream:
+            if (n.measureNumber >= len(list)):
+                for i in range(len(list), n.measureNumber + 1):
+                    list.append([])
+            list[n.measureNumber].append(n)
+        return len(list), list
 
-    def SeperateByNMeasure(self):
-        # TODO
-        return 0
+    def SeperateByNMeasure(self, inputStream, N):
+        nstream = inputStream.recurse().notes.stream()
+        lengthMeasure = 0
+        list = []
+        for n in nstream:
+            print(n.measureNumber)
+            if (n.measureNumber + 1 > lengthMeasure):
+                lengthMeasure = n.measureNumber + 1
+        for i in range(lengthMeasure - N + 1):
+            list.append([])
+        for n in nstream:
+            for i in range(N):
+                index = n.measureNumber - i
+                if(index >= 0) & (index < len(list)):
+                    list[index].append(n)
+        return lengthMeasure, list
 
+    def SeperateByMorceau(self, inputStream, voix, meausreList):
+        length = len(meausreList)
+        startMeasure = meausreList[0]
+        lengthPart, parts = self.SeperateByPart(inputStream)
+        nstream = parts[voix-1].recurse().notes.stream()
+        morceau = []
+        for n in nstream:
+            if (n.measureNumber in meausreList):
+                morceau.append(n)
+        return morceau
+
+# --------------------------------------------------------------------
+# class Translator
+# fonction:
+#     - TranslateToNote
+#     - TranslateToNoteList
+# --------------------------------------------------------------------
 class Translator:
-    def TranslateToNote(self):
-        # TODO
-        return 0
+    def TranslateToNote(self, n, voix):
+        if n.isRest:
+            return Note(voix, -1, -1, 0)
+        elif n.isChord:
+            if n.pitches:
+                return chr(n.pitches[0].midi)
+            else:
+                return chr(127)
+        else:
+            return Note(voix, n.pitch.midi // 12, n.pitch.midi % 12, 0)
 
-    def TranslateToNoteList(self):
-        # TODO
-        return 0
+    def TranslateToNoteList(self, inputList, voix):
+        noteList = []
+        for n in inputList:
+            note = self.TranslateToNote(n, voix)
+            noteList.append(note)
+        return noteList
 
+# --------------------------------------------------------------------
+# class DistanceCalculator
+# fonction:
+#     - Distance
+# --------------------------------------------------------------------
 class DistanceCalculator:
-    def Distance_Note_Note(self, note1, note2):
-        # TODO
-        return 0
+    def Distance(self, target, ref, method):
+        param = '-sequences ' + target + ' ' + ref + ' -method ' + method + ' -result results -type character -parser csv'
+        win32api.ShellExecute(0, 'open', 'MatchingToolBox.exe', param, '', 0)
 
-    def Distance_Note_Accord(self, note, accord):
-        # TODO
-        return 0
-
-    def Distance_Accord_Accord(self, accord1, accord2):
-        # TODO
-        return 0
-
-    def Distance_Morceau_Morceau(self, morceau1, morceau2):
-        # TODO
-        return 0
-
+# --------------------------------------------------------------------
+# class Controller
+# fonction:
+#     - PreprocessMusicFile
+#     - SearchMorceau
+# --------------------------------------------------------------------
 class Controller:
-    def execute(self):
+    def PreprocessMusicFile(self, srcFile, destFolder, N=1):
+        # mode :
+        #   - 1. seperateByMeasure
+        #   - 2. seperateByNMeasure
         fo = FileOperator()
-        fileMei = ABSPATH + r'\data\CRIM\mei\CRIM_Mass_0002_Gloria0.mei'
-        print(fo.ReadFileMEI(fileMei))
-        return 0
+        sp = Seperator()
+        tl = Translator()
+        stream = fo.ReadFile(srcFile)
+        nbParts, parts = sp.SeperateByPart(stream)
+        for i in range(nbParts):
+            voix = i + 1
+            startMeasure = 1
+            nbFile = 1
+            if (N == 1):
+                length, nList = sp.SeperateByMeasure(parts[i])
+            elif(N > 1):
+                length, nList = sp.SeperateByNMeasure(parts[i], N)
+            for list in nList:
+                noteList = tl.TranslateToNoteList(list, voix)
+                filename = ABSPATH + '\\tmp\\target\\' + 'target_' + str(voix) + '_' + str(startMeasure) + '_' + str(nbFile) + '.csv'
+                startMeasure += 1
+                nbFile += 1
+                fo.SaveNoteList(filename, noteList)
+
+    def SearchMorceau(self, srcFile, voix, measureList, distFile):
+        fo = FileOperator()
+        sp = Seperator()
+        tl = Translator()
+        stream = fo.ReadFile(srcFile)
+        noteList = sp.SeperateByMorceau(stream, voix, measureList)
+        morceau = tl.TranslateToNoteList(noteList, voix)
+        filename = ABSPATH + r'\tmp\ref\ref_' + distFile + '.csv'
+        fo.SaveNoteList(filename, morceau)
+
 
 if __name__ == '__main__':
-    Controller().execute()
+    ctl = Controller()
+    STOP = False
+    FileOperator().InitFiles()
+    while(not STOP):
+        print('-------- Choose the function --------')
+        print('1.Create morceau')
+        print('2.Preprocess the muisic file')
+        print('3.Calculate the distance between file and morceau')
+        print('0.Exit')
+        command = input('Please choose your fuction : ')
+        print('you have choosed :', command)
+        if command == '1':
+            srcFile = input('Please input src file path : ')
+            voix = int(input('Please input the voix : '))
+            measureString = input('Please input measures(ex. 3,4,5) : ')
+            measureList = measureString.split(',')
+            for e in measureList:
+                e = int(e)
+            destFile = input('Please input desi file name : ')
+            ctl.SearchMorceau(srcFile, voix, measureList, destFile)
+            print('Morceau created!')
+        elif command == '2':
+            srcFile = input('Please input src file path : ')
+            destFolder = input('Please input the dest floder : ')
+            N = int(input('Please input nb of measures you want to group up : '))
+            ctl.PreprocessMusicFile(srcFile, destFloder, N)
+            print('File analysed!')
+        elif command == '3':
+            targetFolder = input('Please input the target folder : ')
+            targetFolderPath = ABSPATH + '\\tmp\\target\\'
+            method = input('Please input the method : ')
+        elif command == '0':
+            break
